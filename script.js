e2bae5e64a0201daede2d549b50ff992acc338ea
ericamonica <erikalaurindo07@gmail.com>
2021-05-29 10:46:27 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function meuNome(name){
+    console.log("olá meu nome é ${name} ")
+    meunNome("Érica")
+}
+function eu (nome,idade,musica){
+    console.log("olá meu nome é ${nome},eu tenho apenas ${idade},anos, e o melhor estilo musical")
+}
+eu("pop ,33, pop")
+function aboutMe(filme, musica){
+    console.log("Meu filme favorito é ${filme}e a musica que eu amo é $ {musica}")
+}
+    aboutMe("As branquelas", "Até tocar ao céu")
+    
+function triplo(y){
+    return y*3
+}
+    console.log (triplo(10))
